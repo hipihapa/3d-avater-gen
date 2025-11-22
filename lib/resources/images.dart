d8ff5d5cf37392c6c99ff7cfc27d598ea8c8df82
Asumadu Frank Afriyie <afriyiefrankasumadu@gmail.com>
2025-11-22 18:30:11 +0000

    Update image asset paths to use PNG format and add a new background image

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -4,13 +4,14 @@ class Images {
   Images._();
 
   static const String bg = 'images/bg.jpeg';
+  static const String bg2 = 'images/bg2.jpeg';
   static const String circle = 'images/circle.png';
   static const String dot = 'images/dot.png';
-  static const String e1 = 'images/e1.jpeg';
-  static const String e2 = 'images/e2.jpeg';
-  static const String e3 = 'images/e3.jpeg';
-  static const String e4 = 'images/e4.jpeg';
-  static const String e5 = 'images/e5.jpeg';
+  static const String e1 = 'images/e1.png';
+  static const String e2 = 'images/e2.png';
+  static const String e3 = 'images/e3.png';
+  static const String e4 = 'images/e4.png';
+  static const String e5 = 'images/e5.png';
   static const String h1 = 'images/h1.png';
   static const String h2 = 'images/h2.png';
   static const String h3 = 'images/h3.png';
